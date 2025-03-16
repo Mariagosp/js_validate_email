a89b3da7810c7e912c871908b76bf56e6bc92f47
Maria <m.gosp1704@gmail.com>
2025-03-16 18:59:23 +0200

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,51 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(validateEmail('test@mail.com')).toBe(true);
   });
 
   it(`should return 'true' for the valid email`, () => {
     expect(validateEmail('test838@gmail.com.'))
       .toBeTruthy();
   });
-
-  // write more tests here
+  it(`should return 'false' if email doesn't contain English letters (Aa-Zz)`, () => {
+    expect(validateEmail('тест838@gmail.com.'))
+      .toBeFalsy();
+  });
+  it(`should return 'true' if email contains digits`, () => {
+    expect(validateEmail('test678@gmail.com.'))
+      .toBeTruthy();
+  });
+  it(`should return 'true' if email contains '- _'`, () => {
+    expect(validateEmail('te_s-t@gmail.com.'))
+      .toBeTruthy();
+  });
+  it(`should return 'true' if email contains '.' ,
+      provided that it is not the first or last character
+      and it will not come one after the other.`, () => {
+    expect(validateEmail('t.estt@gmail.com.'))
+      .toBeTruthy();
+    expect(validateEmail('t..estt@gmail.com.'))
+      .toBeFalsy();
+    expect(validateEmail('test.@gmail.com.'))
+      .toBeFalsy();
+    expect(validateEmail('.test@gmail.com.'))
+      .toBeFalsy();
+  });
+  it(`should return 'false' if personal_info contains ':'`, () => {
+    expect(validateEmail('te:st@gmail.com.'))
+      .toBeFalsy();
+  });
+  it(`should return 'false' if email doesn't contain @`, () => {
+    expect(validateEmail('testgmail.com.'))
+      .toBeFalsy();
+  });
+  it(`should return 'false' if email doesn't contain @`, () => {
+    expect(validateEmail('testgmail.com.'))
+      .toBeFalsy();
+  });
+  it(`should return 'false' if email contains '! $ % & ' * + / = ? ^ { | } ~'`, () => {
+    expect(validateEmail('te!$stgmail.com.'))
+      .toBeFalsy();
+  });
 });
